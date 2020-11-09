128bbea9e43c04c9b6e227eb989f07910ef971c3
Sébastien Weber <seba.weber@gmail.com>
2020-11-09 12:41:43 +0100

    Initial commit

---
+++ plugin_info.py
@@ -0,0 +1,11 @@
+## To modify by developper(s) of the plugin
+SHORT_PLUGIN_NAME = 'newport'
+package_url = 'https://github.com/CEMES-CNRS/pymodaq_plugins_newport'
+description = 'Set of base PyMoDAQ plugins used with intruments from Newport (Conex, ESP100,...)'
+
+author = 'Sébastien Weber'
+author_email = 'sebastien.weber@cemes.fr'
+
+#packages required for your plugin:
+packages_required = []
+##
