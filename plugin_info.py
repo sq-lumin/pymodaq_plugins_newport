62e248668c5c12604f1598318368e36e2bb8561b
BRESTEAU David <david.bresteau@cea.fr>
2021-08-31 15:42:31 +0200

    Add the plugin for AGILIS AG-UC8

--- plugin_info.py
+++ plugin_info.py
@@ -1,4 +1,4 @@
-## To modify by developper(s) of the plugin
+# To modify by developper(s) of the plugin
 SHORT_PLUGIN_NAME = 'newport'
 package_url = 'https://github.com/CEMES-CNRS/pymodaq_plugins_newport'
 description = 'Set of PyMoDAQ plugins for instruments from Newport (Conex, ESP100,...)'
@@ -7,5 +7,5 @@ author = 'Sébastien Weber'
 author_email = 'sebastien.weber@cemes.fr'
 
 #packages required for your plugin:
-packages_required = []
+packages_required = ['pymodaq', 'easydict', 'pyvisa']
 ##
