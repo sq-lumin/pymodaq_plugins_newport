a27a60161e7be1a6b2c18fafcc249ccdac381e8b
Sébastien Weber <seba.weber@gmail.com>
2023-01-09 15:28:37 +0100

    new layout for plugins

--- src/pymodaq_plugins_newport/daq_move_plugins/daq_move_Newport_ESP100.py
+++ src/pymodaq_plugins_newport/daq_move_plugins/daq_move_Newport_ESP100.py
@@ -1,8 +1,8 @@
-from qtpy.QtCore import QThread
-from pymodaq.control_modules.move_utility_classes import DAQ_Move_base
+
+from pymodaq.control_modules.move_utility_classes import DAQ_Move_base, main
 from pymodaq.daq_move.utility_classes import comon_parameters
 from pymodaq.utils.daq_utils import ThreadCommand, getLineInfo
-from pymodaq_plugins.hardware.Newport.esp100 import ESP100
+from pymodaq_plugins_newport.hardware.esp100 import ESP100
 from easydict import EasyDict as edict
 import pyvisa
 
@@ -41,74 +41,29 @@ class DAQ_Move_Newport_ESP100(DAQ_Move_base):
                         
                         ]}]+comon_parameters
 
-    def __init__(self,parent=None,params_state=None):
-        super().__init__(parent, params_state)
-
-        self.controller=None
-        self.settings.child(('epsilon')).setValue(0.01)
+        
+    def ini_attributes(self):
+        self.settings.child('epsilon').setValue(0.01)
+        self.controller: ESP100 = None
+        
 
     def ini_stage(self, controller=None):
-        """
-            Initialize the controller and stages (axes) with given parameters.
+            
+        self.ini_stage_init(old_controller=controller,
+                            new_controller=ESP100())
+        
+        if self.settings.child('multiaxes','multi_status').value() == "Master":
+            self.controller.init_communication(self.settings.child('com_port').value(), self._axis)
+            
+        controller_id = self.controller.get_controller_infos()
+        self.settings.child('controller_id').setValue(controller_id)
+        self.settings.child('velocity').setValue(self.controller.get_velocity(self._axis))
+        self.settings.child('velocity').setOpts(max=self.controller.get_velocity_max(self._axis))
+        self.settings.child('epsilon').setValue(0.1)
 
-            =============== =========================================== ===========================================================================================
-            **Parameters**   **Type**                                     **Description**
-
-            *controller*     instance of the specific controller object   If defined this hardware will use it and will not initialize its own controller instance
-            =============== =========================================== ===========================================================================================
-
-            Returns
-            -------
-            Easydict
-                dictionnary containing keys:
-                  * *info* : string displaying various info
-                  * *controller*: instance of the controller object in order to control other axes without the need to init the same controller twice
-                  * *stage*: instance of the stage (axis or whatever) object
-                  * *initialized*: boolean indicating if initialization has been done corretly
-
-            See Also
-            --------
-            daq_utils.ThreadCommand
-        """
-
-        # initialize the stage and its controller status
-        # controller is an object that may be passed to other instances of DAQ_Move_Mock in case
-        # of one controller controlling multiaxes
-        try:
-            self.status.update(edict(info="",controller=None,initialized=False))
-
-
-            #check whether this stage is controlled by a multiaxe controller (to be defined for each plugin)
-
-            # if mutliaxes then init the controller here if Master state otherwise use external controller
-            if self.settings.child('multiaxes', 'ismultiaxes').value() and self.settings.child('multiaxes','multi_status').value()=="Slave":
-                if controller is None: 
-                    raise Exception('no controller has been defined externally while this stage is a slave one')
-                else:
-                    self.controller = controller
-            else: #Master stage
-                try:
-                    self.close()
-                except:
-                    pass
-                self.controller = ESP100()
-                self.controller.init_communication(self.settings.child(('com_port')).value(), self._axis)
-
-            controller_id = self.controller.get_controller_infos()
-            self.settings.child(('controller_id')).setValue(controller_id)
-            self.settings.child(('velocity')).setValue(self.controller.get_velocity(self._axis))
-            self.settings.child(('velocity')).setOpts(max=self.controller.get_velocity_max(self._axis))
-            self.settings.child(('epsilon')).setValue(0.1)
-            self.status.info = controller_id
-            self.status.controller = self.controller
-            self.status.initialized = True
-            return self.status
-
-        except Exception as e:
-            self.emit_status(ThreadCommand('Update_Status', [getLineInfo()+ str(e), 'log']))
-            self.status.info = getLineInfo()+ str(e)
-            self.status.initialized = False
-            return self.status
+        info = f'Initialized with controller ID: {controller_id}'
+        initialized = True
+        return info, initialized
 
     def commit_settings(self,param):
         """
@@ -125,7 +80,7 @@ class DAQ_Move_Newport_ESP100(DAQ_Move_base):
         self.controller = None
 
 
-    def check_position(self):
+    def get_actuator_value(self):
         """
             Check the current position from the hardware.
 
@@ -144,9 +99,7 @@ class DAQ_Move_Newport_ESP100(DAQ_Move_base):
         self.emit_status(ThreadCommand('check_position', [pos]))
         return self.target_position
 
-
-
-    def move_Abs(self,position):
+    def move_abs(self,position):
         """
 
         Parameters
@@ -164,7 +117,7 @@ class DAQ_Move_Newport_ESP100(DAQ_Move_base):
         position = self.set_position_with_scaling(position)
         out = self.controller.move_axis('ABS', self._axis, position)
 
-    def move_Rel(self,position):
+    def move_rel(self, position):
         """
             Make the hardware relative move of the Piezo instrument from the given position after thread command signal was received in DAQ_Move_main.
 
@@ -183,9 +136,9 @@ class DAQ_Move_Newport_ESP100(DAQ_Move_base):
         self.target_position = position+self.current_position
         position = self.set_position_relative_with_scaling(position)
 
-        out = self.controller.move_axis('REL', self._axis, pos)
+        out = self.controller.move_axis('REL', self._axis, position)
 
-    def move_Home(self):
+    def move_home(self):
         """
             Move to the absolute vlue 100 corresponding the default point of the Piezo instrument.
 
@@ -204,3 +157,9 @@ class DAQ_Move_Newport_ESP100(DAQ_Move_base):
         move_done
       """
       self.controller.stop_motion()
+
+
+if __name__ == '__main__':
+    
+    main(__file__, init=False)
+    
